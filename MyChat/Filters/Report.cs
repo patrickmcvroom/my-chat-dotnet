863806ff0f4bcc8154b76c03169da427fecea14a
Patrick McVroom <patrickmcvroom@gmail.com>
2021-02-22 21:32:19 +0000

    Removed unnecessary namespace usings

--- MyChat/Filters/Report.cs
+++ MyChat/Filters/Report.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using MindLink.Recruitment.MyChat.Data;
 
 namespace MindLink.Recruitment.MyChat.Filters
--- MyChat/Filters/UserFilter.cs
+++ MyChat/Filters/UserFilter.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using MindLink.Recruitment.MyChat.Data;
 using MindLink.Recruitment.MyChat.Exceptions;
 
